42ce8f3a39f0c0aea2a16361a0015c48315f18ad
lnttran <lamngocthaotran@gmail.com>
2024-09-13 15:24:14 -0500

    Rename to core

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import GraphVisualization from "./graph-visualization/page";
+import GraphVisualization from "./graph-visualization/core";
 import { RouteProvider } from "@/components/context/RouteContext";
 import { CargoProvider } from "@/components/context/CargoContext";
 import { GlobalComponentManagerProvider } from "@/components/context/UIContext";
